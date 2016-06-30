00795a68bcad6ab3f20dc0527176b87e468bf6de
Steve Hudek <sjhudek@gmail.com>
2016-06-30 13:54:01 -0600

    added more images to image gallery

--- client/components/header.js
+++ client/components/header.js
@@ -8,6 +8,15 @@ var app = {
   },
   render: function(){
     $('header').append(navbar);
+    // what page is this
+    // var url = window.location.pathname;
+
+    // based on url, determine which nav item
+    // ...
+
+    // find the nav item for this page
+    // add a class to that nav item
+    // $('ul.nav').eq(1).addClass('active');
   }
 };
 
