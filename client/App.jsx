40587db9c6fafaa2e7feb195a419bef88feb1434
Steve Hudek <sjhudek@gmail.com>
2016-07-24 22:18:31 -0600

    Added D3 Examples submenu to Development link in navbar. Changed Portfolio link to Design in navbar.

--- client/App.jsx
+++ client/App.jsx
@@ -3,7 +3,6 @@ import $ from 'jquery';
 import 'styles/main.scss';
 import home from 'pages/home';
 import todo from 'pages/todo';
-import d3Donutchart from 'pages/d3-donutchart';
 import funnySquares from 'pages/funnySquares';
 import header from 'components/header';
 import footer from 'components/footer';
@@ -26,9 +25,6 @@ $(function(){
     break;
     case '/pages/funnySquares.html':
         funnySquares.init();
-    break;
-    case '/pages/d3-donutchart.html':
-        d3Donutchart.init();
     break;
   }
 
